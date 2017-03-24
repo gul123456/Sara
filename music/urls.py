2871b4e089ea13b446e989f4aec0d6d1ff080fb9
vinitraj10 <ent.vinit01@gmail.com>
2017-03-24 18:17:49 +0530

    Modal Form Added

--- music/urls.py
+++ music/urls.py
@@ -2,10 +2,13 @@ from django.conf.urls import url
 from . import views
 app_name='music'
 urlpatterns = [
-    url(r'^$',views.index,name='index'),
+    url(r'^$',views.IndexView.as_view(),name='index'),
     #/music/album_id/
-    url(r'^(?P<album_id>[0-9]+)/$',views.detail,name='detail'),
-    #/music/album_id/favourite/
-    url(r'^(?P<album_id>[0-9]+)/favorite/$',views.favorite,name='favorite'),
-    
+    url(r'^(?P<pk>[0-9]+)/$',views.DetailView.as_view(),name='detail'),
+    #/music/album_id/favourite/ 
+    url(r'album/add/$',views.AddAlbum.as_view(),name = 'add_album'),
+
+    url(r'album/(?P<pk>[0-9]+)/$',views.UpdateAlbum.as_view(),name = 'update_album'),
+
+    url(r'album/(?P<pk>[0-9]+)/delete/$',views.DeleteAlbum.as_view(),name = 'delete_album'),
 ]
--- music/views.py
+++ music/views.py
@@ -1,24 +1,27 @@
-from django.shortcuts import render,get_object_or_404
+from django.views import generic
+from django.views.generic.edit import CreateView,UpdateView,DeleteView
+from django.core.urlresolvers import reverse_lazy
 from .models import Album,Song
 
-def index(request):
-	all_albums = Album.objects.all()
-	return render(request, 'music/index.html',{'all_albums':all_albums})
+class IndexView(generic.ListView):
+	template_name = 'music/index.html'
+	context_object_name = 'all_albums'
 
-def detail(request,album_id):
-	album = get_object_or_404(Album,pk=album_id)	
-	return render(request, 'music/detail.html', {'album':album})
+	def get_queryset(self):
+		return Album.objects.all()
 
-def favorite(request,album_id):
-	album = get_object_or_404(Album,pk=album_id)
-	try:
-		selected_song = album.song_set.get(pk=request.POST['song'])	
-	except(KeyError,Song.DoesNotExist):
-		return render(request, 'music/detail.html', {
-			'album':album,
-			'error_message':"You did't select any valid song ",
-			})
-	else:
-		selected_song.is_favorite=True
-		selected_song.save()
-	return render(request, 'music/detail.html', {'album':album})
+class DetailView(generic.DetailView):
+	model = Album
+	template_name = 'music/detail.html'
+
+class AddAlbum(CreateView):
+	model = Album
+	fields = ['artist','album_title','album_details','genre','album_logo']
+
+class UpdateAlbum(UpdateView):
+	model = Album
+	fields = ['artist','album_title','album_details','genre','album_logo']
+
+class DeleteAlbum(DeleteView):
+	model = Album
+	success_url = reverse_lazy('music:index')  
